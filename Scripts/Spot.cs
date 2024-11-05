8044f742eea76d51e6665f39dbcbf669b3582b75
cuervo <alex.cuervogrillo@gmail.com>
2024-11-05 07:23:30 -0500

    Add MazeGenerator without loops

---
+++ Scripts/Spot.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spot : MonoBehaviour
+{
+    public int row;
+    public int column;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
